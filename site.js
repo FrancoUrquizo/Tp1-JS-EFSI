1dea18fc7ca6358368011604ebca3e6bae4095e9
FRANCO URQUIZO BARBA <49257286@ort>
2026-03-17 09:52:23 -0300

    ej3 terminado

--- site.js
+++ site.js
@@ -19,30 +19,24 @@ let nuevoTexto = texto.trim().replaceAll(" ", "").length
 
     resultado.innerHTML = "El numero de letras es: " + nuevoTexto;
 }
-
 function NumeroMayor(num1, num2, num3) {
-let NumeroMayor = 0
+  
+    num1 = Number(num1);
+    num2 = Number(num2);
+    num3 = Number(num3);
 
-if (num1 >= numb2 )
-{
-    if(num1 >= num3)
-    {
-NumeroMayor = num1
+    let mayor;
+
+ 
+    if (num1 >= num2 && num1 >= num3) {
+        mayor = num1;
+    } else if (num2 >= num1 && num2 >= num3) {
+        mayor = num2;
+    } else {
+        mayor = num3;
     }
-if(numb3 >= num1)
-{
-    NumeroMayor = numb3
-}
-}
 
-
-if(numb2 >= num3)
-{
-    NumeroMayor = num2
-}
-
-
+   
     const resultado = document.getElementById("resultadoEj3");
-
-    resultado.innerHTML = "El numero mayor: " + NumeroMayor;
+    resultado.innerHTML = "El número mayor es: " + mayor;
 }
